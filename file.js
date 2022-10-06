1664f4e059fd8571ebe46663716b08a793c84b89
Shahabaazpathan <shabazk622@gmail.com>
2022-10-06 21:33:05 +0530

    Extension of file

--- file.js
+++ file.js
@@ -1,6 +1,4 @@
-var side1 = 5; 
-var side2 = 6; 
-var side3 = 7; 
-var s = (side1 + side2 + side3)/2;
-var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
-console.log(area);
+filename = "system.php"
+console.log(filename.split('.').pop());
+filename = "abc.js"
+console.log(filename.split('.').pop());
